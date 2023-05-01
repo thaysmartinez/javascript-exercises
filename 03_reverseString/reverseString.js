b009ee8519dea02f32507950cf3cb91d4c43e084
Thays Martinez <thaysmartinez@gmail.com>
2023-05-01 14:10:47 +0200

    Finish exercise reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function (strOrdered) {
+  const strLength = strOrdered.length;
+  let arrReversed = [];
+  let strReversed;
 
+  for (i = 1; i <= strLength; i++) {
+    arrReversed.push(strOrdered.charAt(strLength - i));
+  }
+  strReversed = arrReversed.toString();
+  strReversed = strReversed.replaceAll(",", "");
+
+  return strReversed;
 };
 
 // Do not edit below this line
