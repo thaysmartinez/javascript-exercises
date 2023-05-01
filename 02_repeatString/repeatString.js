4196984ea2b5e2c1cd52e43ed97b81c9dbfcefb5
Thays Martinez <thaysmartinez@gmail.com>
2023-05-01 13:05:21 +0200

    Complete exercise repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  let wordRepeat = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      wordRepeat += word;
+    }
+    return wordRepeat;
+  }
 };
 
 // Do not edit below this line
